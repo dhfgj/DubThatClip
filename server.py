633079783b2bb42c60919c535f2705120379cae9
Corey <jcshott@gmail.com>
2016-03-05 21:57:22 -0800

    work

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
-from flask import Flask, request, render_template, redirect, flash
+from jinja2 import StrictUndefined
+from flask import Flask, render_template, request, redirect, flash, session, jsonify, g
+from flask_debugtoolbar import DebugToolbarExtension
+
 from get_video_info import get_video_info
 import os
+
+from model import connect_to_db, db, User
 
 app = Flask(__name__)
 app.secret_key = 'abc123'
@@ -10,28 +15,31 @@ app.secret_key = 'abc123'
 def home():
     """This is home page"""
 
+    return render_template("index.html")
 
-
-    return render_template("")
-
-@app.route("/getvoiceover", methods=["POST"]):
+@app.route("/dubIt/api/postrecording", methods=["POST"]):
 def store_voice_over():
     """ grab voice recording and store in db"""
 
-    voice_over_id = request.form.get("voiceTrackId")
-
-
-@app.route("/videoID", methods=["POST"]):
-def pull_video_id():
-    vid_id = request.form.get("id")
-    # 8028026
     
-    vid_info_json = get_video_info(vid_id, 'faces')
+
+@app.route("/dubIt/api/recording", methods=["GET"])
+def get_recording():
+    # get the recording file from db return the file source
+    # return 
+
+# @app.route("/videoID", methods=["POST"]):
+# def pull_video_id():
+#     vid_id = request.form.get("id")
+#     # 8028026
+    
+#     vid_info_json = get_video_info(vid_id, 'faces')
 
 
 
 
 
 if __name__ == "__main__":
+    connect_to_db(app)
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, port=port)
--- static/javascript/voice_file.js
+++ static/javascript/voice_file.js
@@ -1,7 +1,20 @@
 function uploadVoiceTrack(evt) {
 	evt.preventDefault();
 
-	var voiceTrackId =  {"voiceTrackId": //get voice id
+	var voiceLink = $("#voiceRecorder").contents().find("#idDownloadAsMp3Link").href();
+
+	var idxStart, idxEnd;
+
+	for (var i=0; i<= voiceLink.length; i++){
+		if (i === "=") {
+			idxStart = i+1;
+		}
+		if (i === "&") {
+			idxEnd = i;
+		}
+	}
+
+	var voiceTrackId =  {"voiceTrackId": voiceLink.slice(idxStart, idxEnd)}//get voice id
 
 	//send info to server
 	$.post("/getvoiceover", voiceTrackId, function (result) {
